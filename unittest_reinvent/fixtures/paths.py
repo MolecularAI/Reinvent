fe0d70046105b2db3406403df16dca1908df7931
Atanas Patronov <atanas.patronov@astrazeneca.com>
2020-04-04 11:14:41 +0200

    Included model files for integration/unit tests

--- unittest_reinvent/fixtures/paths.py
+++ unittest_reinvent/fixtures/paths.py
@@ -7,7 +7,7 @@ with open(os.path.join(project_root, '../../configs/config.json'), 'r') as f:
 
 MAIN_TEST_PATH = config["MAIN_TEST_PATH"]
 
-RANDOM_PRIOR_PATH = os.path.join(project_root,  "../../unittest_reinvent/fixtures/test_data/augmented.prior")
-SAS_MODEL_PATH = os.path.join(project_root,  "../../unittest_reinvent/fixtures/test_data/SA_score_prediction.pkl")
+RANDOM_PRIOR_PATH = os.path.join(project_root,  "../../data/augmented.prior")
+SAS_MODEL_PATH = os.path.join(project_root,  "../../data/SA_score_prediction.pkl")
 
 
