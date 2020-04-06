b9bfa332548da6a61160e621e0eaf89433296fa8
kdmh016 <christian.margreitter@astrazeneca.com>
2020-04-06 14:52:25 +0200

    Final merge.

--- unittest_reinvent/scoring_tests/synthetic_accessibility/test_sas_component.py
+++ unittest_reinvent/scoring_tests/synthetic_accessibility/test_sas_component.py
@@ -1,5 +1,4 @@
 import unittest
-<<<<<<< HEAD
 import os
 import gzip
 import shutil
@@ -7,27 +6,16 @@ import shutil
 import utils.smiles as chem_smiles
 from scoring.score_components.synthetic_accessibility.sas_component import SASComponent
 from unittest_reinvent.fixtures.paths import MAIN_TEST_PATH, SAS_MODEL_PATH
-=======
-
-import utils.smiles as chem_smiles
-from scoring.score_components.synthetic_accessibility.sas_component import SASComponent
-from unittest_reinvent.fixtures.paths import SAS_MODEL_PATH
->>>>>>> ec9d1e65adeab3dabefb096861877b8a5e7be629
 from unittest_reinvent.scoring_tests.fixtures.predictive_model_fixtures import create_activity_component_regression
 from utils.enums.component_specific_parameters_enum import ComponentSpecificParametersEnum
 
 
-<<<<<<< HEAD
-=======
-# FIXME: remember to unpack the SA_score_prediction.7z for this test to work
->>>>>>> ec9d1e65adeab3dabefb096861877b8a5e7be629
 class Test_sas_component(unittest.TestCase):
 
     def setUp(self):
         csp_enum = ComponentSpecificParametersEnum()
         ts_parameters = create_activity_component_regression()
         ts_parameters.specific_parameters[csp_enum.TRANSFORMATION] = False
-<<<<<<< HEAD
 
         # unzip the model for loading
         if not os.path.isdir(MAIN_TEST_PATH):
@@ -37,15 +25,11 @@ class Test_sas_component(unittest.TestCase):
             with open(tmp_model_path, "wb") as f_out:
                 shutil.copyfileobj(f_in, f_out)
         ts_parameters.model_path = tmp_model_path
-=======
-        ts_parameters.model_path = SAS_MODEL_PATH
->>>>>>> ec9d1e65adeab3dabefb096861877b8a5e7be629
 
         self.query_smiles = ['n1cccc2ccccc12']
         self.query_mols = [chem_smiles.to_mol(smile) for smile in self.query_smiles]
         self.component = SASComponent(ts_parameters)
 
-<<<<<<< HEAD
     @classmethod
     def tearDownClass(self):
         if os.path.isdir(MAIN_TEST_PATH):
@@ -54,9 +38,3 @@ class Test_sas_component(unittest.TestCase):
     def test_sas_1(self):
         summary = self.component.calculate_score(self.query_mols)
         self.assertAlmostEqual(summary.total_score[0], 0.99, 3)
-=======
-    def test_sas_1(self):
-        summary = self.component.calculate_score(self.query_mols)
-        self.assertAlmostEqual(summary.total_score[0], 0.99, 3)
-
->>>>>>> ec9d1e65adeab3dabefb096861877b8a5e7be629
