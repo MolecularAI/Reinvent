6b292b0e68d0354cefaedf521716038e695978cb
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-04-10 15:50:19 +0200

    Use click for CLI
    This just makes things much more streamlined
    
    Also, the user should be responsible for installing their own RDKit, so remove this requirement

--- src/reinvent/cli.py
+++ src/reinvent/cli.py
@@ -2,24 +2,18 @@
 #  coding=utf-8
 
 import json
-import sys
 
-from .model_container import ModelContainer
+import click
+
 from .running_modes.manager import Manager
 
 
-def main():
-    with open(sys.argv[1]) as f:
-        json_input = f.read().replace('\r', '').replace('\n', '')
-
-    configuration = {}
-    try:
-        configuration = json.loads(json_input)
-    except (ValueError, KeyError, TypeError):
-        print("JSON format error")
-    else:
-        manager = Manager(configuration)
-        manager.run()
+@click.command()
+@click.argument('file', type=click.File('r'))
+def main(file):
+    configuration = json.load(file)
+    manager = Manager(configuration)
+    manager.run()
 
 
 if __name__ == "__main__":
