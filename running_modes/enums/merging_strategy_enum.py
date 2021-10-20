21fc4f2f6bbc859806cb61203317b3b3ed22978b
GuoJeff <jeff.guo@outlook.com>
2021-10-20 17:05:22 +0200

    removed unused enums

--- running_modes/enums/merging_strategy_enum.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass(frozen=True)
-class MergingStrategyEnum:
-    LINEAR_MERGE = "linear"
-    LINEAR_MERGE_WITH_THRESHOLD = "linear_with_threshold"
-    LINEAR_MERGE_WITH_BULK = "linear_with_bulk"
-    LINEAR_MERGE_WITH_REMOVAL = "linear_with_removal"
